3f18555169c21c6cedc69fa3d2d20bd963684c67
JACKLEE <dedicatedtools@outlook.com>
2019-03-08 17:11:36 +0800

    项目启动类

---
+++ src/main/java/com/hive/HiveApplication.java
@@ -0,0 +1,18 @@
+package com.hive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ * @author JackLee
+ * @version 1.0
+ * created  2019/3/8 17:06
+ */
+@SpringBootApplication
+public class HiveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HiveApplication.class, args);
+    }
+}
